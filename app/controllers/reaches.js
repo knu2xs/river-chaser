3ec6f3e5ba8f22aa555d6a1c03295c7e9870c872
Joel McCune <knu2xs@gmail.com>
2017-12-06 06:34:44 -0800

    Sort search results

--- app/controllers/reaches.js
+++ app/controllers/reaches.js
@@ -1,12 +1,19 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
 
+  // linking the url to the query params in the field
   queryParams: ['search'],
   search: '',
 
+  // sort the reaches so they appear in logical order
+  sortFields: ['riverName:asc', 'name:asc'],
+  modelSorted: computed.sort('model', 'sortFields'),
+
   actions: {
 
+    // this prevents pressing enter from submitting and reloading the page
     preventSubmit(evt) {
       evt.preventDefault();
       return false;
