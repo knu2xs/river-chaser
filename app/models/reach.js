8b2a5e7703c93c1705f4497736ca92f006a8d5dc
Joel McCune <knu2xs@gmail.com>
2017-12-12 22:07:26 -0800

    Add navigation to reach.index route

--- app/models/reach.js
+++ app/models/reach.js
@@ -17,13 +17,29 @@ export default DS.Model.extend({
   difficultyOutlier: DS.attr('string'),
   dateUpdateAw: DS.attr('date'),
   description: DS.attr('string'),
+  navPutinX: DS.attr('number'),
+  navPutinY: DS.attr('number'),
+  navTakeoutX: DS.attr('number'),
+  navTakeoutY: DS.attr('number'),
 
   // computed properties
   description100: computed('description', function() {
     return this.get('description').split(' ').splice(0,100).join(' ') + '...';
   }),
-  longDescription: computed('description', function() {
-    return (this.get('description').split(' ') >= 100);
+  showDescription100: computed('description', function() {
+    return (this.get('description').split(' ').length >= 100);
+  }),
+  showDetails: computed('description', 'abstract', function(){
+    return this.showDescription100 || this.abstract.length;
+  }),
+  navShuttle: computed('navPutinX', 'navPutinY', 'navTakeoutX', 'navTakeoutY', function(){
+    return `http://maps.google.com/maps?daddr=${this.get('navTakeoutY')},${this.get('navTakeoutX')}&saddr=${this.get('navPutinY')},${this.get('navPutinX')}`
+  }),
+  navPutin: computed('navPutinX', 'navPutinY', function(){
+    return `http://maps.google.com/maps?daddr=${this.get('navTakeoutY')},${this.get('navTakeoutX')}&saddr=Current%20Location`
+  }),
+  navTakeout: computed('navTakeoutX', 'navTakeoutY', function(){
+    return `http://maps.google.com/maps?daddr=${this.get('navPutinY')},${this.get('navPutinX')}&saddr=Current%20Location`
   })
 
 });
